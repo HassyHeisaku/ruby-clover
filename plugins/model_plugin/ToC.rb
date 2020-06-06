45b6e1bde11d2d5e44d0d32ed319e173e8db628d
HassyHeisaku <hassy21@gmail.com>
2020-06-06 15:56:37 +0900

    table of contents are leveled by each section level

--- plugins/model_plugin/ToC.rb
+++ plugins/model_plugin/ToC.rb
@@ -8,11 +8,12 @@ class ToC < ModelPlugin
       toc = ""
       content_after = ""
       count = 0
+      min_hx = content[:contents].scan(/^(\#{1,3}) +/).map{|m| m[0].length}.min
       content[:contents].each_line do |l|
         l.chomp!
-        if((all,hx_title = /\#{1,3} +(.*?) *$/.match(l).to_a)[0] != nil)
+        if((all,hx_length,hx_title = /^(\#{1,3}) +(.*?) *$/.match(l).to_a)[0] != nil)
           content_after += l + " {#a#{count}}\n"
-          toc += "- [#{hx_title}](#a#{count})\n"
+          toc += "  "*(hx_length.length - min_hx) + "- [#{hx_title}](#a#{count})\n"
           count +=1
         else
           content_after += l + "\n"
